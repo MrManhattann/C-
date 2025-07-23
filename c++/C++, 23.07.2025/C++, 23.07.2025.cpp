37fb3cbb3ab6ee4098f2342e2e6b693799980ddb
MrManhattann <95130362+MrManhattann@users.noreply.github.com>
2025-07-23 20:50:50 +0300

    Add files via upload

---
+++ c++/C++, 23.07.2025/C++, 23.07.2025.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+using namespace std;
+/*using std::cout;
+using std::endl;*/
+
+int main() {
+    int a=1, b=2, c=3;
+    int s = 0;
+    s = a + b + c;
+    cout << "Sum of numbers: " << s << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*string name, rename;
+    int age;
+
+    cout << "Enter name: " << endl;
+    cin >> name;
+    cout << "Enter rename: " << endl;
+    cin >> rename;
+    cout << "Enter age: " << endl;
+    cin >> age;
+
+    cout << name << " " << rename << " >> " << age;*/
+
+
+   /*int a = 0;
+    int b=10;
+    int c{ 5 };
+    short d{ 32767 };
+    float f = 12.3;
+    double do1 = 0.001;
+    char ch = 'C';
+    bool isSigned = true;*/ 
+   
+
+
+    /*cout << "Hello!" << endl;
+    cin >> a >> b;
+    cout << "Garbage: " << a << endl;
+    cout << "Garbage: " << b << endl;
+    cout << "Garbage: " << name << endl;
+    cout << "Bool: " << isSigned << endl;*/
+
+   // cout << "Garbage: " << c << endl;
+
+   /* double num1, num2;
+    char op;
+
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter operation (+, -, *, /): ";
+    std::cin >> op;
+
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    double result;
+
+    switch (op) {
+    case '+':
+        result = num1 + num2;
+        std::cout << "Result: " << result << std::endl;
+        break;
+    case '-':
+        result = num1 - num2;
+        std::cout << "Result: " << result << std::endl;
+        break;
+    case '*':
+        result = num1 * num2;
+        std::cout << "Result: " << result << std::endl;
+        break;
+    case '/':
+        if (num2 != 0) {
+            result = num1 / num2;
+            std::cout << "Result: " << result << std::endl;
+        }
+        else {
+            std::cout << "ERORR NULL!" << std::endl;
+        }
+        break;
+    default:
+        std::cout << "ERORR NOT OPERATION!" << std::endl;
+    }*/
+
+    return 0;
+}
